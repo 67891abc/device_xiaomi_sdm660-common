829274bb5be02dbcf1b1dca8fe2b8becf4329a4e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 19:09:53 +0200

    sdm660-common: Remove PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE
    * Deprecated
    
    Change-Id: I1b3638b07ed45f6f72b551d1d241d48f661310c6

--- sdm660.mk
+++ sdm660.mk
@@ -23,7 +23,6 @@
 
 # Inherit properties
 $(call inherit-product, $(LOCAL_PATH)/properties.mk)
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Enable updatable APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
